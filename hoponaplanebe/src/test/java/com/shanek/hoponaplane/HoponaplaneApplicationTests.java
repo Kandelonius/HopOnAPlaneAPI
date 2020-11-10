986dd9afaf357d6ac7042082b60a4bd4738e7dde
Shane Kaestner <shane.kaestner@yahoo.com>
2020-11-09 17:40:40 -0800

    first commit

---
+++ hoponaplanebe/src/test/java/com/shanek/hoponaplane/HoponaplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanek.hoponaplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoponaplaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
