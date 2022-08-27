d6315f4773f23d2526083209e6d58accfcc8ac36
Shane Kaestner <shane.kaestner@yahoo.com>
2022-08-26 20:04:58 -0700

    removed my previously underdeveloped project.

--- hoponaplanebe/src/main/java/com/shanek/hoponaplane/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.shanek.hoponaplane.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message)
-    {
-        super("Error from a Hop on a Plane application " + message);
-    }
-}
