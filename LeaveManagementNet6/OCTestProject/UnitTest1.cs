d7d8dc3f8af412485fbab7868aa97bbdaa00dbcd
Shane Kaestner <shane.kaestner@yahoo.com>
2022-09-01 21:30:37 -0700

    added tests for customer number

--- LeaveManagementNet6/OCTestProject/UnitTest1.cs
+++ LeaveManagementNet6/OCTestProject/UnitTest1.cs
@@ -14,19 +14,37 @@ namespace OCTestProject
         public void Test_Receipt_Number_Cannot_be_Negative()
         {
             long testNumber = -7;
-            RetailStore customer = new RetailStore(testNumber, DateTime.Now, 187, "Frank", "Place", "777-777-7777", 781l, "Great Item", 78m, 3);
+            RetailStore customer = new RetailStore(testNumber, DateTime.Now, 187L, "Frank", "Place", "777-777-7777", 781L, "Great Item", 78m, 3);
 
             Assert.AreEqual(testNumber, customer.ReceiptNumber);
         }
         [TestMethod]
         [TestCategory("Positive")]
-        //[ExpectedException(typeof(AssertFailedException))]
         public void Test_Receipt_Number_Can_be_Positive()
         {
             long testNumber = 7;
-            RetailStore customer = new RetailStore(testNumber, DateTime.Now, 187, "Frank", "Place", "777-777-7777", 781l, "Great Item", 78m, 3);
+            RetailStore customer = new RetailStore(testNumber, DateTime.Now, 187L, "Frank", "Place", "777-777-7777", 781L, "Great Item", 78m, 3);
 
             Assert.AreEqual(testNumber, customer.ReceiptNumber);
+        }
+        [TestMethod]
+        [TestCategory("Negative")]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void Test_Customer_Number_Cannot_be_Negative()
+        {
+            long testNumber = -7;
+            RetailStore customer = new RetailStore(178L, DateTime.Now, testNumber, "Frank", "Place", "777-777-7777", 781L, "Great Item", 78m, 3);
+
+            Assert.AreEqual(testNumber, customer.CustomerNumber);
+        }
+        [TestMethod]
+        [TestCategory("Positive")]
+        public void Test_Customer_Number_Can_be_Positive()
+        {
+            long testNumber = 7;
+            RetailStore customer = new RetailStore(178L, DateTime.Now, testNumber, "Frank", "Place", "777-777-7777", 781L, "Great Item", 78m, 3);
+
+            Assert.AreEqual(testNumber, customer.CustomerNumber);
         }
     }
 }
