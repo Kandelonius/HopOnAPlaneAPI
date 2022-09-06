24d05e36d851ccc44a8464c22db1cb39a521f520
Shane Kaestner <shane.kaestner@yahoo.com>
2022-09-05 19:53:59 -0700

    did my understanding of the section 10 quiz for the C# Selenium course.

---
+++ LeaveManagementNet6/ConsoleQuiz/Program.cs
@@ -0,0 +1,29 @@
+﻿int invalidNumbers = 0;
+int NaNs = 0;
+int[] ints = new int[10];
+string[] given = { "7", "2", "4", "2", "q", "87", "4", "6", "4", "4", "7" };
+for (int i = 0; i < given.Length; i++)
+{
+    int number;
+    bool isParsable = Int32.TryParse(given[i], out number);
+    if (isParsable)
+    {
+        if (number >= 0 && number <= 10)
+        {
+            ints[number]++;
+        }
+        else 
+            invalidNumbers++;
+    }
+    else
+        NaNs++;
+}
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] > 0)
+    {
+        Console.WriteLine($"#{i}:\t{ints[i]}");
+    }
+}
+Console.WriteLine($"invalid:\t{invalidNumbers}");
+Console.WriteLine($"NAN:\t{NaNs}");
