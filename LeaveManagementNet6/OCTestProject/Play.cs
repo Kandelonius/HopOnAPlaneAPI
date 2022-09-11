2283c3115a61015bee4db743f1fb68d8312fea0c
Shane Kaestner <shane.kaestner@yahoo.com>
2022-09-10 21:04:51 -0700

    added properties to the abstract class and added the play class which inherits ticket

---
+++ LeaveManagementNet6/OCTestProject/Play.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCTestProject
+{
+    internal class Play : Ticket
+    {
+        protected string playName;
+        protected bool matinee;
+
+
+    }
+}
--- LeaveManagementNet6/OCTestProject/Ticket.cs
+++ LeaveManagementNet6/OCTestProject/Ticket.cs
@@ -8,13 +8,45 @@ namespace OCTestProject
 {
     abstract class Ticket
     {
-        protected int ticketNumber;
-        protected DateTime eventTime;
-        protected string ticketEvent;
-        protected string seatNumber;
-        protected string eventVenue;
-        protected bool matinee;
-        protected bool adult;
-        protected decimal ticketPrice;
+        private int ticketNumber;
+        private DateTime eventTime;
+        private string ticketEvent;
+        private string seatNumber;
+        private string eventVenue;
+        private bool adult;
+        private decimal ticketPrice;
+
+        protected Ticket(int ticketNumber, DateTime eventTime, string ticketEvent, string seatNumber, string eventVenue, bool adult, decimal ticketPrice)
+        {
+            this.TicketNumber = ticketNumber;
+            this.EventTime = eventTime;
+            this.TicketEvent = ticketEvent;
+            this.SeatNumber = seatNumber;
+            this.EventVenue = eventVenue;
+            this.Adult = adult;
+            this.TicketPrice = ticketPrice;
+        }
+
+        protected int TicketNumber 
+        { 
+            get => ticketNumber; 
+            set 
+            { 
+                ticketNumber = (value > 0)? value : -1;
+            } 
+        }
+        protected DateTime EventTime { get => eventTime; set => eventTime = value; }
+        protected string TicketEvent { get => ticketEvent; set => ticketEvent = value; }
+        protected string SeatNumber { get => seatNumber; set => seatNumber = value; }
+        protected string EventVenue { get => eventVenue; set => eventVenue = value; }
+        protected bool Adult { get => adult; set => adult = value; }
+        protected decimal TicketPrice 
+        { 
+            get => ticketPrice;
+            set 
+            {
+                ticketPrice = (value > 0) ? value : -1; 
+            } 
+        }
     }
 }
