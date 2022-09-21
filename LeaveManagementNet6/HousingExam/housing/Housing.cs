b723b61958ea076c6525862a2039dc8de65b3417
Shane Kaestner <shane.kaestner@yahoo.com>
2022-09-20 19:12:29 -0700

    set up the abstract class for housing with its fields, properties, and constructor. Also added the tests for housing without any body.

---
+++ LeaveManagementNet6/HousingExam/housing/Housing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingExam.housing
+{
+    abstract class Housing
+    {
+        private string address;
+        private string constructionType;
+        private int yearBuilt;
+
+        protected Housing(string address, string constructionType, int yearBuilt)
+        {
+            Address = address;
+            ConstructionType = constructionType;
+            YearBuilt = yearBuilt;
+        }
+
+        public string Address { get => address; set => address = value; }
+        public string ConstructionType { get => constructionType; set => constructionType = value; }
+        public int YearBuilt { get => yearBuilt; set => yearBuilt = value; }
+    }
+}
---
+++ LeaveManagementNet6/HousingExam/tests/HousingTests.cs
@@ -0,0 +1,22 @@
+namespace HousingExam.tests
+{
+    [TestClass]
+    public class HousingTests
+    {
+        [TestMethod]
+        [TestCategory("Housing Test")]
+        public void SingleFamilyTest()
+        {
+        }
+        [TestMethod]
+        [TestCategory("Housing Test")]
+        public void MultyFamilyTest()
+        {
+        }
+        [TestMethod]
+        [TestCategory("Housing Test")]
+        public void CombinedFamilyTest()
+        {
+        }
+    }
+}
